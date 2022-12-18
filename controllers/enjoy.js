8861b98c8cf032f7fcb91f24475342e90889d671
darwin12112 <70789419+darwin12112@users.noreply.github.com>
2022-12-19 00:13:07 +0530

    Update enjoy.js

--- controllers/enjoy.js
+++ controllers/enjoy.js
@@ -329,7 +329,7 @@ var betting = async () => {
 	setTimeout(completing, 150000);
 
 	d = new Date();
-	d = d.getFullYear() + "" + (1 + parseInt(d.getMonth()));
+	d = d.getFullYear() + "" + (1 + parseInt(d.getMonth())) +  parseInt(d.getDate());
 	if (old_d && old_d !== d) {
 		no = 1;
 	}
