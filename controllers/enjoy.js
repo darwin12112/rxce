d622a6eaab3d192ee30d36b47b7681a13cebf6c5
darwin12112 <70789419+darwin12112@users.noreply.github.com>
2022-12-19 00:24:34 +0530

    Update enjoy.js

--- controllers/enjoy.js
+++ controllers/enjoy.js
@@ -329,7 +329,7 @@ var betting = async () => {
 	setTimeout(completing, 150000);
 
 	d = new Date();
-	d = d.getFullYear() + "" + (1 + parseInt(d.getMonth())) +  new Date().toLocaleString("en-US", {timeZone: "asia"});
+	d = d.getFullYear() + "" + (1 + parseInt(d.getMonth())) + 5:30 d.getUTCDate();
 	if (old_d && old_d !== d) {
 		no = 1;
 	}
