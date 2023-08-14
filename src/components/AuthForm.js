9717bfcdde955eeb3ba24b1c0bc219a491b1c201
darwin12112 <70789419+darwin12112@users.noreply.github.com>
2023-08-14 20:27:07 +0530

    Update AuthForm.js

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -25,7 +25,7 @@ class AuthForm extends React.Component {
       confirmPWDErr:false,
       inProgress:false,
       agree:false,
-      verify:'',
+      verify:'false',
       otpProgress:false
     }
   componentWillUnmount() {
