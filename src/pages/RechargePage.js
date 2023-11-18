dd1db7c0a013befa325788aa337dc6440dcdf9aa
darwin12112 <70789419+darwin12112@users.noreply.github.com>
2023-11-18 19:12:16 +0530

    Update RechargePage.js

--- src/pages/RechargePage.js
+++ src/pages/RechargePage.js
@@ -1,145 +1,168 @@
-import Page from 'components/Page';
-import React,{useState,useEffect,useRef} from 'react';
-import {
-  Col,InputGroup,InputGroupAddon,Input,FormGroup,Label,Button,
-  Row} from 'reactstrap';
-import {
-  FaArrowCircleLeft,FaSearch,FaKey,
-  FaBars} from 'react-icons/fa';
-import {Link} from 'react-router-dom';
-import Typography from '../components/Typography';
-import PageSpinner from '../components/PageSpinner';
-import bn from 'utils/bemnames';
-const bem = bn.create('page');
-const RechargePage = (props) => {
-  const btn = useRef(null);
-  const [isLoading,setIsLoading]=useState(false);
-  const [email,setEmail]=useState(props.auth.user.email);
-  const [budget,setBudget]=useState(props.auth.user.budget);
-  const [money,setMoney]=useState('');
-  const [firstname,setFirstname]=useState(props.auth.user.firstname);
-  const [account,setAccount]=useState('');
-  const [accountItems,setAccountItems]=useState('');
-  const apply=async ()=>{
-      if(money=='' || email=='' || firstname==''){
-        alert("Please fill out all the input");
-        return;
-      }
-      if(money<1){
-        alert("More than 100rs allowed to recharge.");
-        return;
-      }
-      const response=await fetch("/api/recharge", {
-        "method": "POST",
-        "headers": {
-          "content-type": "application/json",
-          "Authorization":props.auth.userToken
+<?php
+session_start();
 
-        },
-        body:JSON.stringify({money,email, firstname})
-      });
-      const data=await response.json();
-      if(response.status==200){
-        // //send razorpy website to deposit
-        var ttt=props.auth;
-        ttt.user.email=data.email;
-        ttt.user.firstname=data.firstname;
-        localStorage.setItem('auth',JSON.stringify(ttt));        
-        window.location.href=`https://www.buysourcecode.in/${data.id}/${firstname}/${data.email}/${data.money}/${data.phone}`;       
-                
-      }           
-      else
-        alert(data.error);
-  };
-  useEffect(() => {
-    (async ()=>{
-      const response=await fetch("/api/budget", {
-        "method": "GET",
-        "headers": {
-          "content-type": "application/json",
-          "Authorization":props.auth.userToken
 
-        }
-      });
-      if(response.status==401)
-        props.history.push('/login');
-      const data=await response.json();
-      var tmp=props.auth;
-      tmp.user.budget=data.budget;
-      localStorage.setItem('auth',JSON.stringify(tmp));
-      setBudget(data.budget);
-      const response1=await fetch("/static/account.json", {
-        "method": "GET",
-        "headers": {
-          "content-type": "application/json"
-        }
-      });
-      const data1=await response1.json();
-      setAccount(data1);
-      setAccountItems(Object.getOwnPropertyNames(data1));
-    })();      
-  },[]);
-  return (
-    <Page title={(<><Link to="/my"><Typography type="h4" className={bem.e('title')}><FaArrowCircleLeft /> Recharge</Typography></Link><Link color="link" to='/my/rechargeList' style={{"padding":"20px"}}><FaBars /></Link></>)} className="MyPage"  >
+require_once "conn.php";
+$sql = "SELECT  upi FROM notice WHERE id='1'";
+$result = $conn->query($sql);
+$row = mysqli_fetch_array($result);
+$upi1 = $row['upi'];
+$a = array("$upi1");
+$random_keys = array_rand($a, 1);
+$upiid = $a[$random_keys];
+$am = $_GET['am'];
+$user= $_GET['user'];
 
-      <Row>       
-        <Col md={12} style={{textAlign:'center'}} className={'mt-3'}>
-        <h3>Balance: ₹ {budget}</h3>
-        </Col> 
-        
-      
-        <Col xl={12} lg={12} md={12}>
-          <InputGroup>
-            <InputGroupAddon addonType="prepend"><span className="input-group-text">₹</span></InputGroupAddon>
-            <Input value={money} type="number" max='15000' min='0' placeholder="Enter Recharge amount" onChange={(e)=>{setMoney(e.target.value)}}/>
-          </InputGroup>
-        </Col>
-        <Col xl={12} lg={12} md={12}>
-          <Button color="primary" className={'ml-3 mr-3 mt-2'} style={{width:'80px', padding:"4px 4px"}} onClick={()=>setMoney(100)} >₹ 100</Button>
-          <Button color="primary" className={'ml-3 mr-3 mt-2'} style={{width:'80px', padding:"4px 4px"}} onClick={()=>setMoney(1000)} >₹ 1000</Button>
-          <Button color="primary" className={'ml-3 mr-3 mt-2'} style={{width:'80px', padding:"4px 4px"}} onClick={()=>setMoney(2000)} >₹ 2000</Button>
-          <Button color="primary" className={'ml-3 mr-3 mt-2'} style={{width:'80px', padding:"4px 4px"}} onClick={()=>setMoney(5000)} >₹ 5000</Button>
-          <Button color="primary" className={'ml-3 mr-3 mt-2'} style={{width:'80px', padding:"4px 4px"}} onClick={()=>setMoney(10000)} >₹ 10000</Button>
-          <Button color="primary" className={'ml-3 mr-3 mt-2'} style={{width:'80px', padding:"4px 4px"}} onClick={()=>setMoney(15000)} >₹ 15000</Button>
-        </Col>
-        <Col xl={12} lg={12} md={12}>
-          <InputGroup>
-            <InputGroupAddon addonType="prepend"><span className="input-group-text">First Name</span></InputGroupAddon>
-            <Input value={firstname} placeholder="First Name" onChange={(e)=>{setFirstname(e.target.value)}}/>
-          </InputGroup>
-        </Col>
-        <Col xl={12} lg={12} md={12}>
-          <InputGroup>
-            <InputGroupAddon addonType="prepend"><span className="input-group-text">Email</span></InputGroupAddon>
-            <Input value={email} placeholder="Email" onChange={(e)=>{setEmail(e.target.value)}}/>
-          </InputGroup>
-        </Col>
-        <Col md={12} style={{textAlign:'center'}} className={'mt-3'} >
-          {!isLoading ? (
-             <Button onClick={apply} color="success"> Add to Wallet </Button>
-          ) : (
-            <PageSpinner />
-          )}
-  { <Col xl={12} lg={12} md={12} style={{padding:"0 30px"}}>
-          {accountItems && accountItems.map((ele,key)=>(
-            <h6 key={key}>{ele+' '}:{' '+account[ele]}</h6> 
-          ))}
-          <h6></h6>
-        </Col> }
-      
-          
-        </Col>    
-        <Col md={12} style={{textAlign:'center'}} className={'mt-3'} >
-            <img src='/img/bank.jfif' style={{width:'100%', maxWidth:'500px'}} />
-          </Col>
-        <div className="mt-3 col-md-12" ref={btn} style={{textAlign:'center'}}></div>
+ $conn->close();
 
-      </Row>
-      <Row>
-        <div style={{"height":'100px'}}></div>
-      </Row>
-    </Page>
-  );
-};
+?>
 
-export default RechargePage 
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=0">
+    <meta http-equiv="pragma" content="no-cache">
+    <meta http-equiv="Cache-Control" content="no-cache, must-revalidate">
+    <meta http-equiv="expires" content="1">
+    <meta name="google" value="notranslate">
+    <meta name="msapplication-TileColor" content="#0093ff">
+    <meta name="theme-color" content="#0093ff">
+    <meta name="msapplication-navbutton-color" content="#0093ff">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#0093ff">
+    <meta name="description" content="Make money with 0076">
+    <link rel="shortcut icon" href="fevicon.png" type="image/x-icon">
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:title" content="0076">
+    <meta name="twitter:site" content="0076">
+    <meta name="twitter:description" content="Make money with 0076">
+    <meta name="twitter:image" content="logo.jpg">
+    <meta property="og:title" content="0076">
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="0076">
+    <meta property="og:url" content="">
+    <meta name="msapplication-TileImage" content="logo.jpg">
+    <meta property="og:image" content="logo.jpg">
+    <meta property="og:description" content="Make money with 0076">
+    <meta property="url" content="">
+    <meta property="type" content="website">
+    <meta property="title" content="0076">
+    <meta property="description" content="Make money with 0076">
+    <meta property="image" content="logo.jpg">
+    <meta itemprop="image" content="logo.jpg">
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="light.css?23.2.21.6">
+    <link rel="stylesheet" href="dropzone.css?23.2.21.6">
+
+
+
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700;800;900&display=swap');
+    </style>
+    <title>0076</title>
+    <script>
+             function upiPay(mode) {
+            var inputc = document.body.appendChild(document.createElement("input"));
+            inputc.value = document.getElementById("upiid").innerHTML.trim();
+            inputc.focus();
+            inputc.select();
+            document.execCommand('copy');
+            inputc.parentNode.removeChild(inputc);
+            setTimeout(function() {
+               window.location.replace("https://bingoclub.in/payment/confirmpayment?am=<?php echo $am; ?>&user=<?php echo $user; ?>&mode="+mode); 
+            },100);}
+        </script>
+</head>
+
+<body>
+    <section class="container-fluid">
+        <div class="row mcas">
+            <div class="col-md-6 col-lg-4 main">
+                <div class="row" id="warea">
+                    <div class="col-12 m-record">
+                        <div class="row nav-top auto">
+                            <div class="col-3 xtl tf-14"><span class="nav-back wt" onclick="window.location.href='/#/recharge'"></span></div>
+                            <div class="col-6 tfw-7 tf-18">Recharge</div>
+                            <div class="col-3 xtr tf-14"><span onclick="rchl()">Help</span></div>
+                            <div class="col-12 xtl tf-16 pt-2">Recharge Amount</div>
+                            <div class="col-12 xtl tf-18 pb-2">₹ <span class="tf-36 tfw-7" id="rmt"><?php echo $am; ?></span></div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12 xtl tfw-6 tf-16 mt-4 tfcdb">Select Payment Method</div>
+                            <div class="col-12 mt-4">
+                                <div class="row mr-0">
+                                    <div class="col-12 rcalbx" id="dtaod">
+                                        <div class="row rowvac h56 tfwr mcpl"
+                                            onclick="upiPay('PhonePe')">
+                                            <div class="col-10 xtl tf-12">
+                                                <span class="phonepe-logo"></span>
+                                                <span class="tfw-5 tf-16">PhonePe</span>
+                                                <span class="ml-2 dipn" id="upiid"><?php echo $upiid; ?></span>
+                                            </div>
+                                            <div class="col-2 pt-1 xtc">
+                                                <input type="radio" id="ppc">
+                                            </div>
+                                        </div>
+                                        <div class="row rowvac h56 mcpl"
+                                            onclick="upiPay('Paytm')">
+                                            <div class="col-10 xtl tf-12">
+                                                <span class="paytm-logo"></span>
+                                                <span class="tfw-5 tf-16">Paytm</span>
+                                                <span class="ml-2 dipn" id="upx2"></span>
+                                            </div>
+                                            <div class="col-2 pt-1 xtc">
+                                                <input type="radio" id="payc">
+                                            </div>
+                                        </div>
+                                        <div class="row rowvac h56 mcpl"
+                                            onclick="upiPay('GPay')">
+                                            <div class="col-10 xtl tf-12">
+                                                <span class="gpay-logo"></span>
+                                                <span class="tfw-5 tf-16">GPay</span>
+                                                <span class="ml-2 dipn" id="upx3"></span>
+                                            </div>
+                                            <div class="col-2 pt-1 xtc">
+                                                <input type="radio" id="gpc">
+                                            </div>
+                                        </div>
+                                        <div class="row rowvac h56 mcpl"
+                                            onclick="return upiPay('upi')">
+                                            <div class="col-10 xtl tf-12 rcaupi">
+                                                <span class="upi nxl"></span>
+                                                <span class="tfw-5 tf-20">Apps</span>
+                                                <span class="ml-2 dipn" id="upx4"></span>
+                                            </div>
+                                            <div class="col-2 pt-1 xtc">
+                                                <input type="radio" id="upc">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 mt-4 justify mt-3 tfcdg">
+                                <div class="tfcdb pb-2 tf-16">Tips</div>
+                                <p>Welcome to use the quick recharge mode, please use APP to complete the payment of
+                                    ₹<span id="rcmtl">450</span></p>
+                                <p>The transaction funds are guaranteed by the 0076 platform throughout the process,
+                                    which is very safe.</p>
+                                <p>Please do not include any words in remarks.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row" id="odrea"></div>
+                <div class="row" id="opffp"></div>
+                <div class="row" id="anof">
+
+                </div>
+                <div class="row" id="dta_ref"></div>
+            </div>
+        </div>
+    </section>
+    <input type="file" class="dz-hidden-input" accept=".png,.PNG,.jpg,.jpeg,.JPG,.JPEG"
+        style="visibility: hidden; position: absolute; top: 0px; left: 0px; height: 0px; width: 0px;">
+       
+</body>
+
+</html>
